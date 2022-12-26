b92955df8cb9d274206aef676644536213f3551d
Rouan van der Ende <rouan.vanderende@iotnxt.com>
2022-12-26 14:52:29 +0200

    day4 part1

---
+++ day4/run.ts
@@ -0,0 +1,40 @@
+console.log("day4");
+// const data = Deno.readTextFileSync("sample.txt")
+const data = Deno.readTextFileSync("part1data.txt")
+  .trim()
+  .split("\n")
+  .map((line, pairId) => {
+    const elves = line.split(",").map((elf, elfId) => {
+      const s = elf.split("-").map((s) => {
+        return parseInt(s);
+      });
+      const from = s[0];
+      const to = s[1];
+      const range = to - from + 1;
+      return { elfId, pairId, from, to, range };
+    });
+
+    return { pairId, elves };
+  });
+
+console.log(data);
+
+function fullyContain(pair: typeof data[number]) {
+  const [a, b] = pair.elves;
+  console.log(`Checking`);
+  console.log(a);
+  console.log(b);
+
+  if (a.from <= b.from && a.to >= b.to) return true;
+  if (b.from <= a.from && b.to >= a.to) return true;
+
+  return false;
+}
+
+let count = 0;
+data.forEach((i) => {
+  const result = fullyContain(i);
+  if (result) count++;
+});
+
+console.log(`Answer is: ${count}`);
